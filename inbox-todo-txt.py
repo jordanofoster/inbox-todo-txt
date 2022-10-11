e8f165c4b3947057506029e71330bbf8ee2670eb
Jordan Foster <JordanOFoster@outlook.com>
2022-10-11 22:15:26 +0000

    Initial files didn't commit?

---
+++ inbox-todo-txt.py
@@ -0,0 +1,103 @@
+import os, argparse, glob, yaml, re, logging
+from datetime import datetime
+
+parser = argparse.ArgumentParser(description="Program that populates a todo.txt file based on reading contents.")
+parser.add_argument('yaml', type=str)
+parser.add_argument('-td', '--todo', type=str)
+parser.add_argument('-i', '--inbox', type=str)
+parser.add_argument('-exp', '--expression-type', choices=('regex','glob'), required=True, type=str)
+parser.add_argument('--debug', action='store_true')
+args = parser.parse_args()
+print(args)
+
+def isMatch(exp, str, expType=args.expression_type):
+  if expType == 'regex':
+    try:
+      pattern = re.compile(r'{}'.format(exp))
+      if re.match(pattern, str):
+        return True
+    except re.error:
+      logging.warning(f"Regexp - {exp} considered invalid expression")
+  elif expType == 'glob':
+      if str in glob.glob(exp):
+        return True
+
+  if args.debug:
+    logging.debug(f"Match not found in {str} with expression {exp} (type: {expType})")
+  return False
+
+try:
+  with open(args.yaml, 'r') as yamlFile:
+    yamlContents = yaml.safe_load(yamlFile)
+    yamlFields = yamlContents.keys()
+except yaml.YAMLError as yamlE:
+  logging.critical(yamlE)
+  exit(1)
+except FileNotFoundError as fnfE:
+  logging.critical(fnfE)
+  exit(1)
+
+if args.todo == None:
+  try:
+    todo_dir = yamlContents['todo_dir']
+  except KeyError as ke:
+    logging.critical(ke)
+    exit(1)
+else:
+  todo_dir = args.todo
+
+if args.inbox == None:
+  try:
+    inbox_dir = yamlContents['inbox_dir']
+  except KeyError as ke:
+    logging.critical(ke)
+    exit(1)
+
+try:
+  with open(todo_dir, "rt") as todoDump:
+    todoInitialContents = todoDump.read()
+except FileNotFoundError:
+  todoInitialContents = ""
+  
+with open(todo_dir, "at") as todoAppend:
+  for filedir in glob.glob(inbox_dir + "*"):
+      filename = os.path.basename(filedir)
+      if isMatch(todo_dir,filedir,'glob') or isMatch(args.yaml,filename,'glob') or isMatch(__file__,filedir,'glob'):
+        continue
+      if filename not in todoInitialContents:
+        ignored = False
+          
+        for exp in yamlContents['ignore']:
+              if isMatch(exp,filename):           
+                ignored = True
+                break
+
+        if ignored == False:
+
+          outputStr = ""
+          
+          projectsStr = ""
+          for project,expList in yamlContents['projects'].items():
+            for exp in expList:
+              if isMatch(exp,filename):     
+                if projectsStr != "":
+                  projectsStr += " "
+                projectsStr += f"+{project}"
+                break
+                    
+          contextsStr = ""
+          for context,expList in yamlContents['contexts'].items():
+            for exp in expList:
+              if isMatch(exp,filename):
+                if contextsStr != "":
+                  contextsStr += " "
+                contextsStr += f"@{context}"
+                break
+          
+          outputStr = f"{datetime.now().strftime('%G-%m-%d')} {filename}"
+          if projectsStr != "":
+            outputStr += " " + projectsStr
+          if contextsStr != "":
+            outputStr += " " + contextsStr
+
+          todoAppend.write(f"{outputStr}\n")
